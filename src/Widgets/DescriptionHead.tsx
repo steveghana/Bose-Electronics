50fcc5855a5e4a66517d13d32f4a727e5b79078a
unknown <boatengstephen707@gmail.com>
2022-11-05 13:00:11 -1100

    inserted product description

---
+++ src/Widgets/DescriptionHead.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DescriptionHead = () => {
+  return (
+    <div className="description__header">
+      <h1>Bose Dynamic</h1>
+      <div>Lorem ipsum</div>
+    </div>
+  );
+};
+
+export default DescriptionHead;
--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BudsOntable from "../assets/designs/pexels-callum-hilton-7856680.jpg";
 import Kelly from "../assets/designs/pexels-philip-boakye-2614384.jpg";
+import DescriptionHead from "../Widgets/DescriptionHead";
 function Description() {
   return (
     <div className="description__container">
@@ -8,7 +9,23 @@ function Description() {
         <div className="description__left-top"></div>
         <div className="description__left-bottom"></div>
       </div>
-      <div className="description__center">center</div>
+      <div className="description__center">
+        <DescriptionHead />
+        {new Array(2).fill("").map((item, i) => (
+          <div className="description__text-container">
+            <div className="description__img">.</div>
+            <div className="description__text-wrapper" key={i}>
+              <h4>Bose something</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, dignissimos deleniti asperiores cum exercitationem
+                voluptate.
+              </p>
+              <div className="date"></div>
+            </div>
+          </div>
+        ))}
+      </div>
       <div className="description__right">
         <div className="absolute"></div>
       </div>
